7e7aed1213403dd1b5d878061ae267655eacc266
Freaklord11 <141641366+Freaklord11@users.noreply.github.com>
2024-07-06 22:04:01 +0800

    Initial commit

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import css from './Button.module.css';
+
+export default class Button extends Component {
+  static propTypes = {
+    onClick: PropTypes.func.isRequired,
+  };
+
+  render() {
+    return (
+      <button className={css.Button} onClick={this.props.onClick}>
+        Load more
+      </button>
+    );
+  }
+}
---
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import ImageGalleryItem from '../ImageGalleryItem/ImageGalleryItem';
+import css from './ImageGallery.module.css';
+
+export default class ImageGallery extends Component {
+  static propTypes = {
+    images: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.number.isRequired,
+      })
+    ).isRequired,
+  };
+
+  render() {
+    const { images } = this.props;
+    return (
+      <ul className={`${css.gallery} js-gallery`}>
+        {images.map(image => (
+          <ImageGalleryItem key={image.id} image={image} />
+        ))}
+      </ul>
+    );
+  }
+}
